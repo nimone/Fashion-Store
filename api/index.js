2f668b9a02d53c3c1a02f64be2b25b5790735fc6
nimone <nishantmogha1234@gmail.com>
2021-10-10 12:09:21 +0530

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+
 const app = express()
+
+dotenv.config()
+
+mongoose.connect(process.env.DB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => console.log("Connected to database"))
+	.catch(err => console.error(err))
+
 
 app.get("/", (req, res) => {
 	res.json({status: "ok"})
 })
 
-app.listen(5000, () => console.log("Listening on localhost:5000"))
+app.listen(process.env.PORT || 5000, () => {
+	console.log(`Listening on port ${process.env.PORT || 5000}`)
+})
