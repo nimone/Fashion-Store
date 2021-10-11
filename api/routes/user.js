5bad44871a29ccfbf4077b1765b9cdd40d1e63c7
nimone <nishantmogha1234@gmail.com>
2021-10-11 12:58:36 +0530

    add basic user routes with user stats

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,138 @@
 const router = require("express").Router()
+const bcrypt = require("bcryptjs")
+const User = require("../models/User.model")
+const { 
+	verifyToken,
+	verifyAuthorization,
+	verifyAdminAccess,
+} = require('../middlewares/verifyAuth')
 
-router.get("/:id", (req, res) => {
-	res.json({userid: req.params.id})
+
+
+// Get all users - admin only
+router.get("/", verifyAdminAccess, async (req, res) => {
+	const query = req.query
+	try {
+		let users 
+		if (query.new) {
+			users = await User
+				.find({}, { password: 0 }) // exclude password
+				.sort({ createdAt: -1 })
+				.limit(5)
+		} else {
+			users = await User.find({}, { password: 0 })
+		}
+		return res.json(users)
+
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
 })
 
-router.post("/create", (req, res) => {
-	const { username } = req.body
-	console.log(username)
-	res.status(201).json({message: `${username} created`})
+// Get current user - any authenticated user
+router.get("/me", verifyToken, async (req, res) => {
+	try {
+		const user = await User.findById(
+			req.user.uid,
+			{ password: 0 },
+		)
+		return res.json(user)
+
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
 })
+
+// Update a user - authorized user & admin only
+router.put("/:id", verifyAuthorization, async (req, res) => {
+	let { password, fullname } = req.body
+
+	if (password) {
+		password = await bcrypt.hash(password, 10)
+	}
+
+	try {
+		await User.findByIdAndUpdate(
+			req.params.id,
+			{$set: { fullname, password } },
+			{new: true},
+		)
+		return res.json(userResponse.userUpdated)
+		
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
+})
+
+// Delete a user - authorized user & admin only
+router.delete("/:id", verifyAuthorization, async (req, res) => {
+	try {
+		await User.findByIdAndDelete(req.params.id)
+		res.json(userResponse.userDeleted)
+
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
+})
+
+// Get user statistics - admin only
+router.get("/stats", verifyAdminAccess, async (req, res) => {
+	const date = new Date()
+	const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
+
+	try {
+		const data = await User.aggregate([
+			{$match: {
+				createdAt: { $gte: lastYear },
+			}},
+			{$project: {
+				month: { $month: "$createdAt" },
+			}},
+			{$group: {
+				_id: "$month",
+				total: { $sum: 1},
+			}}
+		])
+		res.json(data)
+
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
+})
+
+// Get any user - admin only
+router.get("/:id", verifyAdminAccess, async (req, res) => {
+	try {
+		const user = await User.findById(
+			req.params.id, 
+			{ password: 0 },
+		)
+		return res.json(user)
+
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json(userResponse.unexpectedError)
+	}
+})
+
+const userResponse = {
+	unexpectedError: {
+		status: "error",
+		message: "an unexpected error occurred",
+	},
+	userDeleted: {
+		status: "ok",
+		message: "user has been deleted",
+	},
+	userUpdated: {
+		status: "ok",
+		message: "user has been updated",
+	}
+}
 
 module.exports = router
