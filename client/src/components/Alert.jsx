8e9d54e8806ce32234839918e807256ede5c5966
nimone <nishantmogha1234@gmail.com>
2021-10-24 13:04:04 +0530

    Add alert

---
+++ client/src/components/Alert.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import clsx from "clsx"
+
+export default function Alert({ heading, body, info, success, danger, warning }) {
+	return (
+		<div 
+			class={clsx(
+				"rounded-lg px-4 py-3 mb-4 text-sm",
+				info && "text-blue-700 bg-blue-100",
+				danger && "text-red-700 bg-red-100",
+				success && "text-green-700 bg-green-100",
+				warning && "text-yellow-700 bg-yellow-100",
+			)}
+			role="alert"
+		>
+		  <span class="font-medium">{heading}</span> {body}
+		</div>
+	)
+}
