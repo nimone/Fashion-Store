b92e4d49c4f60633559819a8a8aecd03310e3923
nimone <nishantmogha1234@gmail.com>
2021-10-25 15:04:11 +0530

    add image background to login and register pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { sliderItems } from '@/dummydata'
 
 import LoginForm from "@/ui/LoginForm"
 
@@ -6,10 +7,14 @@ export default function LoginPage() {
 	const handleLogin = userData => {
 		console.log(userData)
 	}
-	
+	const randomSlide = sliderItems[Math.floor(Math.random() * sliderItems.length)]
+
 	return (
-		<main className="flex justify-center h-screen items-center bg-gray-300">
-			<div className="min-w-sm p-6 rounded-lg bg-white">
+		<main 
+			className="flex justify-center h-screen items-center bg-cover bg-center sm:bg-left"
+			style={{backgroundImage: `url(${randomSlide.image})`}}
+		>
+			<div className="min-w-sm p-6 rounded-lg bg-white filter drop-shadow-2xl">
 				<h3 className="text-2xl font-bold text-center mb-6">Login to your account</h3>
 				<LoginForm onSubmit={handleLogin} />
 			</div>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { sliderItems } from '@/dummydata'
 
 import RegisterForm from "@/ui/RegisterForm"
 
@@ -7,9 +8,14 @@ export default function RegisterPage() {
 		console.log(userData)
 	}
 
+	const randomSlide = sliderItems[Math.floor(Math.random() * sliderItems.length)]
+
 	return (
-		<main className="flex justify-center h-screen items-center bg-gray-300">
-			<div className="min-w-sm p-6 rounded-lg bg-white">
+		<main 
+			className="flex justify-center h-screen items-center bg-cover bg-center sm:bg-left"
+			style={{backgroundImage: `url(${randomSlide.image})`}}
+		>
+			<div className="min-w-sm p-6 rounded-lg bg-white filter drop-shadow-2xl">
 				<h3 className="text-2xl font-bold text-center mb-6">Create a new account</h3>
 				<RegisterForm onSubmit={handleRegister}/>
 			</div>
