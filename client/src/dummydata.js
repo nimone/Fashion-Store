f7d7ac4fb7061c547419f6c0663e1672bdf11e16
nimone <nishantmogha1234@gmail.com>
2021-10-25 13:22:39 +0530

    add more dummy data and only show 4 products on popular section

--- client/src/dummydata.js
+++ client/src/dummydata.js
@@ -15,7 +15,7 @@ export const sliderItems = [
   },
   {
     id: 3,
-    image: "https://images.pexels.com/photos/1300550/pexels-photo-1300550.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    image: "https://images.pexels.com/photos/999267/pexels-photo-999267.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "FORMAL WEAR",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
@@ -57,33 +57,49 @@ export const popularProducts = [
   {
     id:1,
     image:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    name: "Jake Guitar Vintage Crusher Tee",
+    price: 20,
   },
   {
     id:2,
     image:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    name: "Angela Natural Tee",
+    price: 25,
   },
   {
     id:3,
     image:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    name: "Double Match Heavy Cotton Shirt",
+    price: 30,
   },
   {
     id:4,
     image:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    name: "Cotton Dress",
+    price: 30,
   },
   {
     id:5,
     image:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    name: "Noissue X Creatsy Tote Bag",
+    price: 10,
   },
   {
     id:6,
     image:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    name: "Rocket Vintage Chill Cap",
+    price: 8,
   },
   {
     id:7,
-    image:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    image:"https://www.prada.com/content/dam/pradanux_products/2/2TE/2TE183/3LJ6F0964/2TE183_3LJ6_F0964_SLR.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.400.500.webp",
+    name: "White Leather high-top Sneakers",
+    price: 50,
   },
   {
     id:8,
     image:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    name: "Women Jacket",
+    price: 65,
   },
 ]
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { ChevronRight } from "react-feather"
 import { categories, popularProducts, sliderItems } from '@/dummydata'
 
+import Button from "@/components/Button"
 import CategoryList from "@/ui/CategoryList"
 import ProductList from "@/ui/ProductList"
 import Newsletter from "@/ui/Newsletter"
@@ -20,7 +23,13 @@ export default function HomePage() {
 
 			<section className="my-14">
 				<h2 className="text-3xl font-medium">Popular Products</h2>
-				<ProductList products={popularProducts} />
+				<ProductList products={[...popularProducts].splice(0, 4)} />
+
+				<Link to="/products" className="flex justify-center">
+					<Button className="text-lg mt-6" link>
+						View More <ChevronRight className="ml-2" />
+					</Button>
+				</Link>
 			</section>
 
 			<section className="my-20">
