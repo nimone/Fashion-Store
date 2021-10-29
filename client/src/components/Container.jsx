b9eab8246caf4cce1bc07e1e60a59de31b4ec7df
nimone <nishantmogha1234@gmail.com>
2021-10-29 13:44:53 +0530

    make Container and PageHeader more reusable

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Container({ heading, children }) {
+import PageHeader from "./PageHeader"
+
+export default function Container({ heading, children, type="section" }) {
 	return (
 		<section className="my-14 container mx-auto">
-			<h2 className="text-3xl font-medium mb-6 mx-4">{heading}</h2>
+			<PageHeader h2={type !== "page"}>
+				{heading}
+			</PageHeader>
+			<hr className="h-px w-1/2 mx-auto bg-gray-300 mt-3 mb-8" />
 			{children}
 		</section>
 	)
--- client/src/components/PageHeader.jsx
+++ client/src/components/PageHeader.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function PageHeader({ children }) {
-	return (
-		<h1 className="text-center font-light text-4xl md:text-5xl">
-			{children}
-		</h1>
-	)
+export default function PageHeader({ children, h2, h3, className }) {
+	const commonClasses = `text-center font-light ${className}`
+
+	if (h2) {
+		return <h2 className={`text-3xl md:text-4xl ${commonClasses}`}>{children}</h2>
+	}	
+	if (h3) {
+		return <h3 className={`text-2xl md:text-3xl ${commonClasses}`}>{children}</h3>
+	}
+
+	return <h1 className={`text-4xl md:text-5xl ${commonClasses}`}>{children}</h1>
 }
