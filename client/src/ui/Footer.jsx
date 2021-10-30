4ee742a2564dd7c9abdcd829ed9ab07b9c258224
nimone <nishantmogha1234@gmail.com>
2021-10-30 14:00:06 +0530

    update footer links

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -5,7 +5,7 @@ import { Facebook, Instagram, Twitter, MapPin, Phone, Mail } from "react-feather
 export default function Footer() {
 	return (
 		<footer className="grid grid-cols-1 md:(grid-cols-3) border-t border-gray-300 bg-gray-200 px-4">
-			<div className="m-6 flex-1">
+			<div className="m-4 sm:m-6 flex-1">
 				<h2 className="text-4xl text-center md:text-left mb-4">BRAND</h2>
 				<p className="text-justify text-gray-700">Eiusmod duis reprehenderit quis cillum nisi anim consectetur occaecat cupidatat anim incididunt aliqua eiusmod ad consectetur in ut cupidatat proident dolore aute irure enim in in ut adipisicing in do est.</p>
 				<ul className="flex mt-6 justify-center md:justify-start space-x-4">
@@ -20,36 +20,36 @@ export default function Footer() {
 					</li>
 				</ul>
 			</div>			
-			<div className="m-6">
+			<div className="m-4 sm:m-6">
 				<h2 className="text-xl text-center md:text-left font-medium mb-4">Useful Links</h2>
 				<ul className="flex flex-col flex-wrap h-36 space-y-1 text-gray-600">
 					<li>
-						<Link to="#">Home</Link>
+						<Link to="/">Home</Link>
 					</li>
 					<li>
-						<Link to="#">Cart</Link>
+						<Link to="/cart">Cart</Link>
 					</li>
 					<li>
-						<Link to="#">Men Fashion</Link>
+						<Link to="/products?category=men">Men Fashion</Link>
 					</li>
 					<li>
-						<Link to="#">Women Fashion</Link>
+						<Link to="/products?category=women">Women Fashion</Link>
 					</li>
 					<li>
-						<Link to="#">Track Order</Link>
+						<Link to="/orders">Track Order</Link>
 					</li>
 					<li>
-						<Link to="#">My Account</Link>
+						<Link to="/account">My Account</Link>
 					</li>							
 					<li>
-						<Link to="#">Wishlist</Link>
+						<Link to="/wishlist">Wishlist</Link>
 					</li>					
 					<li>
-						<Link to="#">Terms</Link>
+						<Link to="/terms">Terms</Link>
 					</li>
 				</ul>
 			</div>			
-			<div className="m-6">
+			<div className="m-4 sm:m-6">
 				<h2 className="text-xl text-center md:text-left font-medium mb-4">Contact</h2>
 				<ul className="space-y-3 text-gray-700">
 					<li className="flex items-center">
@@ -62,7 +62,9 @@ export default function Footer() {
 					</li>					
 					<li className="flex items-center">
 						<Mail className="w-5 h-5 mr-2" />
-						<span>contact@brand.com</span>
+						<Link to="mailto:contact@brand.com">
+							contact@brand.com
+						</Link>
 					</li>
 				</ul>
 				<div className="mt-6">
