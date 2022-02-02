578954a8255ca43e902daf3fb9d6316c6d1e7494
nimone <nishantmogha1234@gmail.com>
2022-02-02 11:28:33 +0530

    fix cart reducer logic

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -26,37 +26,31 @@ export default function cartReducer(state, action) {
       let removedProduct
       return {
         ...state, 
-        products: state.products.filter(product => {
-          if (product._id === action.payload) {
+        products: state.products.filter(p => {
+          if (p.id === action.payload) {
             removedProduct = p
-            return true
+            return false
           }
-          return false
+          return true
         }),
-        total: cartTotal - removedProduct.price
+        total: removedProduct 
+          ? cartTotal - removedProduct.price * removedProduct.quantity
+          : cartTotal,
       }
-    case "INCREMENT_PRODUCT_QUANTITY":
-      let newProducts = [...state.products]
-      productIdx = newProducts.findIndex(p => p._id === action.payload.id)
-      newProducts[productIdx].quantity++
+    case "SET_PRODUCT_QUANTITY":
+      if (action.payload.quantity < 1) {
+        return state
+      }
+      const newProducts = [...state.products]
+      const productIdx = newProducts.findIndex(p => p.id === action.payload.id)
+      const prevQuantity = newProducts[productIdx].quantity
+      newProducts[productIdx].quantity = action.payload.quantity
 
       return {
         ...state,
         products: newProducts,
-        total: cartTotal + newProducts[productIdx].price
+        total: cartTotal + (action.payload.quantity - prevQuantity) * newProducts[productIdx].price,
       }
-    case "DECREMENT_PRODUCT_QUANTITY":
-      newProducts = [...state.products]
-      productIdx = newProducts.findIndex(p => p._id === action.payload.id)
-      if (newProducts[productIdx].quantity > 1) {
-        newProducts[productIdx].quantity--
-        return {
-          ...state,
-          products: newProducts,
-          total: cartTotal - newProducts[productIdx].price
-        }
-      }
-      return state
     default:
       return state
   }
