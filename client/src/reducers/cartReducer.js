288152a5c8be7266337ffe727218087abf39bf11
nimone <nishantmogha1234@gmail.com>
2022-02-12 13:07:46 +0530

    simplify & refactor cart reducer

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -3,74 +3,65 @@ export const initialCartState = {
   total: 0
 }
 
-export default function cartReducer(state, action) {
-  let cartTotal = state.total
+function getCartTotal(products) {
+  return products.reduce((sum, p) => sum + p.price * p.quantity, 0)
+}
 
+export default function cartReducer(state, action) {
+  let newProducts
+  
   switch(action.type) {
-    case"RESET":
-      return {
-        products: [],
-        total: 0,
-      }
+    case "RESET":
+      return initialCartState
+
     case "SET_PRODUCTS":
-      for (let product of action.payload) {
-        cartTotal += product.quantity * product.price
-      }
       return {
         ...state, 
         products: action.payload,
-        total: cartTotal,
+        total: getCartTotal(action.payload),
       }
+
     case "ADD_PRODUCTS":
-      console.log(action.payload)
+      newProducts = [
+        ...state.products, 
+        ...action.payload.map(product => ({
+          id: product._id,         
+          title: product.title,
+          price: product.price,
+          image: product.image,
+          quantity: product.quantity,
+        })),
+      ]
+
       return {
         ...state,
-        products: [
-          ...state.products, 
-          ...action.payload.map(product => ({
-            id: product._id,         
-            title: product.title,
-            price: product.price,
-            image: product.image,
-            quantity: product.quantity,
-          })),
-        ],
-        total: cartTotal + action.payload.reduce((sum, p) => sum+p.price,0)
+        products: newProducts,
+        total: getCartTotal(newProducts)
       }
+
     case "REMOVE_PRODUCT":
-      let removedProduct
+      newProducts = state.products.filter(p => p.id !== action.payload)
+
       return {
         ...state, 
-        products: state.products.filter(p => {
-          if (p.id === action.payload) {
-            removedProduct = p
-            return false
-          }
-          return true
-        }),
-        total: removedProduct 
-          ? cartTotal - removedProduct.price * removedProduct.quantity
-          : cartTotal,
+        products: newProducts,
+        total: getCartTotal(newProducts),
       }
+
     case "SET_PRODUCT_QUANTITY":
       if (action.payload.quantity < 1) {
         return state
       }
-      const newProducts = [...state.products]
+      newProducts = [...state.products]
       const productIdx = newProducts.findIndex(p => p.id === action.payload.id)
-      const prevQuantity = newProducts[productIdx].quantity
       newProducts[productIdx].quantity = action.payload.quantity
 
       return {
         ...state,
         products: newProducts,
-        total: cartTotal + (action.payload.quantity - prevQuantity) * newProducts[productIdx].price,
+        total: getCartTotal(newProducts),
       }
-    case "CLEAR_CART":
-      return {
-        products: [],
-        total: 0,
-      }
+
     default:
       return state
   }
